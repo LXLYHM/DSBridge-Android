4fd024340e6dd2a13760fe9145f1c961d11e5ad3
CaoPengfei <whiskeyfei@users.noreply.github.com>
2017-10-27 11:28:02 +0800

    fixed

--- dsbridge/src/main/java/wendu/dsbridge/DWebView.java
+++ dsbridge/src/main/java/wendu/dsbridge/DWebView.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.Keep;
@@ -523,7 +522,7 @@ public class DWebView extends WebView {
         }
     };
     private void injectJs() {
-        evaluateJavascript("function getJsBridge(){window._dsf=window._dsf||{};return{call:function(b,a,c){\"function\"==typeof a&&(c=a,a={});if(\"function\"==typeof c){window.dscb=window.dscb||0;var d=\"dscb\"+window.dscb++;window[d]=c;a._dscbstub=d}a=JSON.stringify(a||{});return window._dswk?prompt(window._dswk+b,a):\"function\"==typeof _dsbridge?_dsbridge(b,a):_dsbridge.call(b,a)},register:function(b,a){\"object\"==typeof b?Object.assign(window._dsf,b):window._dsf[b]=a}}}dsBridge=getJsBridge();");
+        evaluateJavascript("function getJsBridge(){window._dsf=window._dsf||{};return{call:function(b,a,c){\"function\"==typeof a&&(c=a,a={});if(\"function\"==typeof c){window.dscb=window.dscb||0;var d=\"dscb\"+window.dscb++;window[d]=c;a._dscbstub=d}a=JSON.stringify(a||{});return window._dswk?prompt(window._dswk+b,a):\"function\"==typeof _dsbridge?_dsbridge(b,a):_dsbridge.callAndroid(b,a)},register:function(b,a){\"object\"==typeof b?Object.assign(window._dsf,b):window._dsf[b]=a}}}if(typeof(window.dsBridge)!='undefined'){console.log('window.dsBridge is exist!')}else{window.dsBridge=getJsBridge();initBridge();}");
     }
 
     private void _evaluateJavascript(String script) {
